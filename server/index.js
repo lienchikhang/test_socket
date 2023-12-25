367dce4d310fdb72890f32406d9481cc4789b507
lienchikhang <lienchikhang777@gmail.com>
2023-12-25 15:29:00 +0700

    update

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ app.use(express.static('/public'));
 
 const server = require('http').createServer(app);
 
+
+app.get('/', (req, res) => {
+    res.status(200).json({success:true})
+})
+
 const io = new Server(server, {
     cors: {
         origin: 'http://localhost:3000',
